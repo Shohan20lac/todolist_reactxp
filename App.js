1823cd27b25c0acc29388d0f2a206bdb101faa20
shohan20lac <r.shohan12@gmail.com>
2023-11-18 20:22:46 +0600

    Initialized dev environment

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, SafeAreaViewComponent, StyleSheet, Text, View } from 'react-native';
+import Task from './components/task';
+import { SafeAreaView } from 'react-native';
+import { Button } from 'react-native';
+import { useState } from 'react';
+
+
+const XpContainer = ({ platform, children }) => {
+  return platform === "web" ? (
+    <View style={styles.container}>
+      <Text> Web </Text>
+      {children}
+    </View>
+  ) : (
+    <SafeAreaView style={[styles.container, styles.containerMobile]}>
+      <Text> Mobile </Text>
+      {children}
+    </SafeAreaView>
+  );
+};
+
+
 
 export default function App() {
+  const [tasks, setTasks] = useState([])
+  
+  const addTask = (text) => {
+    setTasks(
+      [
+        ...tasks, 
+        { text }
+      ]
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <XpContainer platform={Platform.OS}>
+      <View style={styles.tasksWrapper}> 
+        <Text style={styles.sectionTitle}> Today's Tasks </Text>
+      </View>
+      <View style={styles.itemsContainer}>
+        {
+          tasks.length == 0 ? (
+            <Text>
+              No Notes Yet
+            </Text>
+          ) : (
+              tasks.map((task, index) => (
+                <Task key={index} text={task.text} />
+              ))
+          ) 
+        }
+      </View>
+    </XpContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "E8EAED",
+    alignItems: 'center'
   },
+  containerMobile: {
+    marginTop: 50,
+    paddingHorizontal: 20
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  sectionTitleContainer: {
+    backgroundColor: 'white'
+  },
+  itemsContainer: {
+    flex:1,
+    justifyContent: 'center'
+  }
 });
---
+++ components/task.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Task = () => {
+  return (
+    <View>
+        <Text> This is a task. </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create ({
+    
+})
+
+export default Task
